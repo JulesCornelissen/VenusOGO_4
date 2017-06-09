7742a487007dbe3f703b0f47f899fe984bb4a67a
teards <teards@nooby.eu>
2017-06-09 09:56:57 +0200

    Updated the IR receive code so that it now dumps the value in to a 32bit integer for further use

--- Arduino/Beacon/multipilerecv/multipilerecv.ino
+++ Arduino/Beacon/multipilerecv/multipilerecv.ino
@@ -8,31 +8,49 @@
 //for a more efficient way.
 #include "IRLibAll.h"
 
-IRrecvPCI recvOne(7); //create receiver and pass pin number
-IRrecvPCI recvTwo(3); //create second receiver on pin 3
+IRrecvPCI recvOne(0); //create receiver and pass pin number
+IRrecvPCI recvTwo(1); //create second receiver
+IRrecvPCI recvThree(7); //create third receiver
 IRdecode myDecoder;   //create decoder
 
+uint32_t IRValueOne;
+uint32_t IRValueTwo;
+uint32_t IRValueThree;
+  
 void setup() {
   Serial.begin(9600);
   delay(2000); while (!Serial); //delay for Leonardo
   recvOne.enableIRIn(); // Start the receiver
-  recvTwo.enableIRIn(); // start second receiver
+  recvTwo.enableIRIn(); // Start second receiver
+  recvThree.enableIRIn(); // Start third receiver
   Serial.println(F("Ready to receive IR signals"));
 }
 
 void loop() {
-  //Continue looping until you get a complete signal received
+     /*
+     * Change these three functions to compare the data and then output
+     * which quadrant it's actually in. 
+     */
   if (recvOne.getResults()) {
     Serial.println(F("---------------------------------------\n RECEIVER ONE \n---------------------------------------"));
-    myDecoder.decode();           //Decode it
-    myDecoder.dumpResults(true);  //Now print results. Use false for less detail
-    recvOne.enableIRIn();      //Restart receiver
+    //myDecoder.decode();           //Decode it
+    //myDecoder.dumpResults(true);  //Now print results. Use false for less detail
+    IRValueOne = myDecoder.value; //Store the received value in a 32bit integer
+    recvOne.enableIRIn();     //Restart receiver
   }
   if (recvTwo.getResults()) {
     Serial.println(F("---------------------------------------\n RECEIVER TWO \n---------------------------------------"));
-    myDecoder.decode();           //Decode it
-    myDecoder.dumpResults(true);  //Now print results. Use false for less detail
-    recvTwo.enableIRIn();      //Restart receiver
+    //myDecoder.decode();           //Decode it
+    //myDecoder.dumpResults(true);  //Now print results. Use false for less detail
+    IRValueTwo = myDecoder.value; //Store the received value in a 32bit integer
+    recvTwo.enableIRIn();     //Restart receiver
+  }
+  if (recvThree.getResults()) {
+    Serial.println(F("---------------------------------------\n RECEIVER THREE \n---------------------------------------"));
+    //myDecoder.decode();           //Decode it
+    //myDecoder.dumpResults(true);  //Now print results. Use false for less detail
+    IRValueThree = myDecoder.value; //Store the received value in a 32bit integer
+    recvThree.enableIRIn();   //Restart receiver
   }
 }
 
